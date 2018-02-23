afb3e0981faf8c1f1f0ecbe212cfcfb6ab63272a
Dave Thomas <kukulcanenator@googlemail.com>
2018-02-23 10:00:28 +0000

    Added occurs check, bind, unify/many, solver
    Note the use of nested modules due to no type classes in F#
    Could this use SRTP or an interface here?  Maybe, although we want parity with thr haskell implementation first before evolving.

--- HMSplitSolve/HMSplitSolve.fs
+++ HMSplitSolve/HMSplitSolve.fs
@@ -1,5 +1,8 @@
 module HMSplitSolve
 open System
+open System.Data
+open System.Collections.Generic
+open System.Data
 
 
 type TVar = TV of String
@@ -75,6 +78,7 @@ type InferState() =
 
 
 type Constraint = (Typ * Typ)
+type ConstraintList = Constraint list
 
 type Subst = Map<TVar,Typ>
 
@@ -111,13 +115,35 @@ module Constraint =
   let rec ftv ((t1, t2): Constraint) =
     Set.union (Typ.ftv t1) (Typ.ftv t2) 
 
+module List = 
+  let private apply apply (s: Subst) xs  =
+    List.map (apply s) xs
+    
+  let private ftv ftv (xs: Collections.Generic.KeyValuePair<'a,'b> list)  =
+    List.foldBack (fun (KeyValue(_key ,v)) state ->
+      Set.union state (ftv v)) xs Set.empty
+
+  module Scheme =
+    let apply = apply Scheme.apply
+    let ftv = ftv Scheme.ftv
+
+  module Constraint =
+    let apply = apply Constraint.apply
+    let ftv = ftv Constraint.ftv
+
+  module Typ =
+    let apply = apply Typ.apply
+    let ftv = ftv Typ.ftv
+
 module Env =
   let apply s env =
     Map.map (fun _k v -> Scheme.apply s v) env
 
   let ftv env =
-    Seq.foldBack (fun (KeyValue(_key ,v)) state ->
+    List.foldBack (fun (KeyValue(_key ,v)) state ->
       Set.union state (Scheme.ftv v)) env Set.empty
+
+
 
 type TypeError =
   | UnificationFail of Typ * Type
@@ -161,6 +187,38 @@ let (++) = List.append
 let compose (s1:Subst) (s2: Subst) : Subst =
   Map.union (Map.map (fun _k v -> Typ.apply s1 v) s2) s1
 
+let occursCheck a t =
+  Set.contains a (Typ.ftv t)
+
+let bind a t =
+  match a, t with
+  | a, t when (TVar a) == t  -> Map.empty
+  | a, t when occursCheck a t -> failwithf "InfiniteType %A %A"  a t
+  | _otherwise -> Map.singleton a t
+
+let rec unifyMany ts1 ts2 =
+  match ts1, ts2 with
+   | [], [] -> Map.empty
+   | (t1 :: ts1), (t2 :: ts2) ->
+     let su1 = unifies t1 t2
+     let su2 = unifyMany (List.Typ.apply su1 ts1) (List.Typ.apply su1 ts2)
+     compose su2 su1
+   | t1, t2 -> failwithf "UnificationMismatch: %A %A" t1 t2
+
+and unifies t1 t2 =
+  match t1, t2 with
+  | t1, t2 when t1 == t2 -> Map.empty
+  | TVar v, t | t, TVar v -> bind v t
+  | TArr(t1, t2), TArr(t3, t4) -> unifyMany [t1; t2] [t3; t4]
+  | t1, t2 -> failwithf "UnificationFail: %A %A" t1 t2
+
+let rec solver ((su, cs) : Unifier) =
+  match cs with
+    | [] ->  su
+    | ((t1, t2) :: cs0) -> 
+      let su1  = unifies t1 t2
+      solver (compose su1 su, List.Constraint.apply su1 cs0)
+
 let rec infer expr env inferState : Typ * Constraint list =
   match expr with
   | Lit(LInt _) -> (typeInt, [])
@@ -180,6 +238,8 @@ let rec infer expr env inferState : Typ * Constraint list =
     let tv = fresh inferState
     
     (tv, c1 ++ c2 ++ [t1, TArr(t2, tv)] )
+
+
 
 //let inferExpr env expr : Result<Scheme, TypeError> =
   
