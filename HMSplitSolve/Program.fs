164c90d6cb6fc93292feeac73b0f51702c65b5c1
Dave Thomas <kukulcanenator@googlemail.com>
2018-02-26 22:47:21 +0000

    Add basic inference test and print inference and constraints

--- HMSplitSolve/Program.fs
+++ HMSplitSolve/Program.fs
@@ -1,8 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open HMSplitSolve
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let basicInference =
+      Let ("id", Lam("x", Var "x"), Var "id")
+
+    let result = inferTop TypeEnv.empty ["basicInference", basicInference] (InferState())
+    printfn "Inference: %A" result
+
+    let constraints, subst, typ, scheme = constraintsExpr basicInference TypeEnv.empty (InferState())
+    printfn "Constraints: %A" constraints
+    printfn "Substitutions: %A" subst
+    printfn "Type: %A" typ
+    printfn "Scheme: %A" scheme
     0 // return an integer exit code
