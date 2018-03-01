5a93da96e4b73146d614df9b37c56d6ab495f4a1
Dave Thomas <kukulcanenator@googlemail.com>
2018-03-01 20:53:04 +0000

    Add test banks

--- HMSplitSolve/Program.fs
+++ HMSplitSolve/Program.fs
@@ -17,8 +17,26 @@ let main argv =
       Let("id", Lam( "x", App(Var "x", Var "x")), Var "id")
     let b6 =
       Lam("m", (Let("y", Var "m", Let("x", (App(Var "y", Lit(LBool true))), Var "x"))))
+    ///fun f -> (fun g -> (fun arg (f g arg)))"
     let b7 =
       Lam("f", Lam("g", Lam("arg", App(Var("g"), App(Var("f"), Var("arg"))))))
+
+    let testBank = [b1; b2; b3; b4; b6; b7]
+    //warmup
+    testBank |> List.iter (fun t -> ignore <| inferTop TypeEnv.empty ["t", t] (InferState()) )
+    
+    let runTestBank bank =
+      List.iter (fun t -> ignore <| inferTop TypeEnv.empty ["t", t] (InferState()) ) bank
+
+    let times =
+      [for i in 1..1000 ->
+         let sw = Diagnostics.Stopwatch.StartNew()
+         runTestBank testBank
+         sw.Stop()
+         sw.Elapsed.TotalMilliseconds ]
+
+    printfn "Average bank = %f" (times |> List.average)
+    printfn "Average individual = %f" (times |> List.averageBy (fun t -> t / 6.0))
 
     let result = inferTop TypeEnv.empty ["b7", b7] (InferState())
     printfn "Inference: %A" result
