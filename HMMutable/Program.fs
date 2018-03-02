6e62f4725b8da704e6091b2c794d6eb6b02fd48d
Dave Thomas <kukulcanenator@googlemail.com>
2018-03-02 23:58:59 +0000

    Add tests and main

---
+++ HMMutable/Program.fs
@@ -0,0 +1,83 @@
+﻿open System
+open HMMutable
+
+[<EntryPoint>]
+let main argv =
+
+    // let b1 =
+    //     "b1", ELet ("id", EAbs("x", EVar "x"), EVar "id")
+
+    // let b2 =
+    //     "b2", ELet ("id", EAbs( "x", EVar "x"), EApp(EVar "id", EVar "id"))
+
+    // let b3 =
+    //     "b3", ELet( "id", (EAbs("x", ELet( "y", EVar "x", EVar "y"))), EApp(EVar "id", EVar "id"))
+
+    // let b4 =
+    //     "b4", ELet( "id", EAbs( "x", ELet("y", EVar "x", EVar "y")),    EApp(EApp(EVar "id", EVar "id") ,EPrim (Int 2)))
+
+    // let b5 =
+    //     "b5", ELet("id", EAbs( "x", EApp(EVar "x", EVar "x")), EVar "id")
+
+    // let b6 =
+    //     "b6", EAbs("m", (ELet("y", EVar "m", ELet("x", (EApp(EVar "y", EPrim(Bool true))), EVar "x"))))
+
+    // let b7 =
+    //       "b6: fun f -> (fun g -> (fun arg (f g arg)))",
+    //       EAbs("f", EAbs("g", EAbs("arg", EApp(EVar("g"), EApp(EVar("f"), EVar("arg"))))))
+
+    let example1 =
+        "let f = fun x y -> g(x, y) in f(a, b)",
+        Fun([("a")], Var(("a") ))
+
+    let example2 =
+        "compose or (>>)",
+        Let("compose",
+            Fun(["f"], 
+                Fun(["g"], 
+                    Fun (["x"], 
+                        Call(Var "g",[Call(Var "f", [Var "x"])])
+                    )
+                )
+            ), Var "compose"
+        )
+
+    let example3 =
+        "fun (a,b) -> a",
+        Fun(["a"; "b"], Var(("a") ))
+
+    let example4 =
+        "let f a b = a",
+        Fun( ["a"], Fun(["b"], Var "a"))
+
+    let example5 =
+        "fun x -> let y = x in y",
+        Fun(["x"], Let("y", Var "x", Var "y"))
+
+
+
+    let testBank = [example1; example2; example3; example4; example5]
+
+    let runTestBank bank printResult =
+      bank
+      |> List.iter (fun (name, exp) -> resetId()
+                                       let ty = infer Env.empty 0 exp
+                                       let generalizedTy = generalize (-1) ty 
+                                       if printResult then
+                                            printfn "%s" name
+                                            printfn "Expression: %s" (HMMutable.exp.toString exp)
+                                            printfn "Infered: %s" (HMMutable.ty.toString generalizedTy) )
+
+    //run through the tests as a warm up
+    runTestBank testBank true
+
+    let times =
+      [for i in 1..1000 ->
+         let sw = Diagnostics.Stopwatch.StartNew()
+         runTestBank testBank false
+         sw.Stop()
+         sw.Elapsed.TotalMilliseconds ]
+
+    printfn "Average bank = %f" (times |> List.average)
+    printfn "Average individual = %f" (times |> List.averageBy (fun t -> t / float testBank.Length ))
+    0 // return an integer exit code
