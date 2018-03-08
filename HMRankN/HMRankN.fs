a53586f7bdf2098de09f3cb19fce878a9e17834a
Dave Thomas <kukulcanenator@googlemail.com>
2018-03-08 23:14:38 +0000

    More progress on rank N alg

--- HMRankN/HMRankN.fs
+++ HMRankN/HMRankN.fs
@@ -1,7 +1,6 @@
 module HMRankN
 open System
 open ExtCore
-open System.Diagnostics
 
 type Name = String
 type Uniq = int
@@ -17,7 +16,7 @@ type Typ =
   | Fun of  Typ * Typ        // Function type
   | TyCon of TyCon             // Type constants
   | TyVar of TyVar             // Always bound by a ForAll
-  | MetaTv of MetaTv           // A meta type variable
+  | MetaTv of MetaTv          // A meta type variable
 
 and Rho = Typ //No top-level ForAll
 and Tau = Typ //No ForAlls anywhere
@@ -30,6 +29,17 @@ and TyRef =
 
 and MetaTv = 
   | Meta of Uniq * TyRef // Can unify with any tau-type
+
+module MetaTv =
+  let equals m1 m2 =
+    match m1, m2 with Meta(u1, _), Meta(u2, _) -> u1 = u2
+
+module TyVar =
+  let equals t1 t2 =
+    match t1, t2 with
+    | BoundTv s1, BoundTv s2 -> s1 = s2
+    | SkolemTv(_, u1), SkolemTv(_, u2) -> u1 = u2
+    | _ -> false
 
 type Term =
   | Var of Name
@@ -64,3 +74,20 @@ let metaTvs types =
     | Fun(arg, res), acc -> go arg (go res acc)
     | ForAll(_, ty), acc -> go ty acc //ForAll binds TyVars only
   List.foldBack go types []
+
+let freeTyVars tys =
+  let rec go bound ty acc =
+    match bound, ty, acc with
+    | bound, TyVar tv, acc ->
+      match tv with
+      | tv when List.contains tv bound -> acc
+      | tv when List.contains tv acc -> acc
+      | _otherwise -> tv :: acc
+    | bound, MetaTv _, acc -> acc
+    | bound, TyCon _, acc -> acc
+    | bound, Fun(arg, res), acc -> go bound arg (go bound res acc)
+    | bound, ForAll(tvs, ty), acc -> go (tvs ++ bound) ty acc
+    
+
+  List.foldBack (go []) tys []
+
