0ebd6c80462336606338165aa2f25d08b09c6c18
Dave Thomas <kukulcanenator@googlemail.com>
2018-03-16 17:52:12 +0000

    Add difference to the prelude, matching the haskell spec

--- HMRankN/Prelude.fs
+++ HMRankN/Prelude.fs
@@ -1,3 +1,12 @@
 [<AutoOpenAttribute>]
 module List
     let (++) = List.append
+
+    let rec deleteByEq xs x =
+        match xs with
+        | [] -> []
+        | ( y :: ys) ->
+            if x = y then ys else y :: deleteByEq ys x
+
+    let difference xs1 xs2=
+        List.fold deleteByEq xs1 xs2
